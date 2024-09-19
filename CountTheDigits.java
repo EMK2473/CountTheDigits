0454d812902e0ade0194696e291d7c4a8b130b23
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-19 12:35:03 -0400

    defined countDigit method; handled case for 0 input as base 1 digit case; handled case for 1 digit case

--- CountTheDigits.java
+++ CountTheDigits.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class CountTheDigits {
    
-   /* TODO: Write recursive digitCount() method here. */
+   public static int digitCount(int intInput) {
+      if (intInput == 0){
+         return 1;
+      }
+      if(intInput / 10 == 0) {
+         return 1;
+      }
+      return 1;
+   }
    
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
